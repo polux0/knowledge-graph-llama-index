9e5e22b8956b236d2af508bc77cabbca28b9c92d
polux0 <afidelo@protonmail.com>
2024-02-07 19:25:03 +0100

    feat: knowledge graph constructed with 15 triplets per chunk & Mistral used as LLM;

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -16,6 +16,8 @@ from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
 from llama_index.embeddings import LangchainEmbedding
 from pyvis.network import Network
 
+
+
 # load environment variables from .env file
 load_dotenv()
 
@@ -29,7 +31,7 @@ logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 # setup the LLM
 llm = HuggingFaceInferenceAPI(
-    model_name="HuggingFaceH4/zephyr-7b-beta", token=HF_TOKEN
+    model_name="mistralai/Mistral-7B-Instruct-v0.2", token=HF_TOKEN
 )
 
 # setup up Embedding model 
@@ -59,21 +61,22 @@ print('Constructing the Knowledge Graph Index...')
 
 # load the Knowlege Graph Index ( if it's not first application )
 try:
-  storage_context = StorageContext.from_defaults(persist_dir='./persistence/real_world_community_model')
+  storage_context = StorageContext.from_defaults(persist_dir='./persistence/real_world_community_model_15_triplets_per_chunk')
   index = load_index_from_storage(storage_context=storage_context,
                                   service_context=service_context,
-                                  max_triplets_per_chunk=3,
+                                  max_triplets_per_chunk=15,
                                   include_embeddings=True)
   index_loaded = True
   print('Loading of index is finished...')
 except:
   index_loaded = False
+  print('Index not found, constructing one...')
 if not index_loaded:
   # construct the Knowledge Graph Index
   
   storage_context = StorageContext.from_defaults(graph_store=graph_store)
   index = KnowledgeGraphIndex.from_documents(documents=documents,
-                                             max_triplets_per_chunk=3,
+                                             max_triplets_per_chunk=15,
                                              service_context=service_context,
                                              storage_context=storage_context,
                                              include_embeddings=False)
@@ -84,7 +87,7 @@ print('Construction of index is finished...')
 
 # persist the Knowledge Graph Index so we don't have to recreate it again once we want to play with it
 
-storage_context.persist('./persistence/real_world_community_model')
+storage_context.persist('./persistence/real_world_community_model_15_triplets_per_chunk')
 
 # technical debt - create script from here that loads from persisted index
 
@@ -98,7 +101,12 @@ storage_context.persist('./persistence/real_world_community_model')
 
 # query = "What is reality?"
 
-query = "What is Real world community model?"
+# query = "What is Real world community model?"
+# Answer, using Mistral-7B-Instruct-v0.2 as LLM
+# The Real World Community Model is a concept described in the Aurvana System Overview document. It refers to a model of a community as a population of people navigating together in common, surrounded by their environment, and includes the direction domain (goals and other directional components), data domain (data collected from the environment and data output as the result of information processing), knowledge domain (logical integration of observations and relationships into common knowledge, social approach, and the value system and reasoning), and values domain (society's value system and reasoning that supports the fulfillment of needs and orients the total alignment with the common direction of intent). The model is dynamically simulatable and adaptive, and the whole of the real world must be modeled for optimal societal structuring.
+
+query = "What are the domains of the Real World Commuity Model?"
+
 
 # Related to questions about `real world community model`
 
