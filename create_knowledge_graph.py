4a9488d3a6393664831150b8237b6d94eaaf4dd2
polux0 <afidelo@protonmail.com>
2024-02-15 11:12:43 +0100

    feat: minimalistic front-end;

--- create_knowledge_graph.py
+++ create_knowledge_graph.py
@@ -93,43 +93,39 @@ storage_context.persist('./persistence/real_world_community_model_15_triplets_pe
 
 # query the knowledge graph by building Query Engine
 
-# Related to questions about flow
-
-# query = "What does it feel like to be in a state of the flow?"
-# query = 'What is the ‘flow’ cycle?'
-# query = 'What are the five performance enhancing chemicals that are released during a flow state?'
-
-# query = "What is reality?"
-
-# query = "What is Real world community model?"
-# Answer, using Mistral-7B-Instruct-v0.2 as LLM
-# The Real World Community Model is a concept described in the Aurvana System Overview document. It refers to a model of a community as a population of people navigating together in common, surrounded by their environment, and includes the direction domain (goals and other directional components), data domain (data collected from the environment and data output as the result of information processing), knowledge domain (logical integration of observations and relationships into common knowledge, social approach, and the value system and reasoning), and values domain (society's value system and reasoning that supports the fulfillment of needs and orients the total alignment with the common direction of intent). The model is dynamically simulatable and adaptive, and the whole of the real world must be modeled for optimal societal structuring.
 
 query = "What are the domains of the Real World Commuity Model?"
 
 
 # Related to questions about `real world community model`
-
-query_engine = index.as_query_engine(include_text=True,
+def generate_response_based_on_question(question):
+  query_engine = index.as_query_engine(include_text=True,
                                      response_mode ="tree_summarize",
                                      embedding_mode="hybrid",
                                      similarity_top_k=5,)
-#
-message_template =f"""<|system|>Please check if the following pieces of context has any mention of the keywords provided in the Question.
+
+  message_template =f"""<|system|>Please check if the following pieces of context has any mention of the keywords provided in the Question.
                                 If not then don't know the answer, just say that you don't know.
                                 Stop there.
                                 Please do not try to make up an answer.</s>
-<|user|>
-Question: {query}
-Helpful Answer:
-</s>"""
-#
-response = query_engine.query(message_template)
-#
-print(response.response.split("<|assistant|>")[-1].strip())
+                                <|user|>
+                                Question: {question}
+                                Helpful Answer:
+                                </s>"""
 
-# visualizing the graph
-g = index.get_networkx_graph()
-net = Network(notebook=True, cdn_resources="in_line", directed=True)
-net.from_nx(g)
-net.show("real_world_community_model.html")
+  response = query_engine.query(message_template)
+
+  # print response as it was initially
+  # print(response.response.split("<|assistant|>")[-1].strip())
+
+  # print whole response without splitting
+  print(response)
+  return response.response
+
+def vizualize_created_graph():
+
+  # visualizing the graph
+  g = index.get_networkx_graph()
+  net = Network(notebook=True, cdn_resources="in_line", directed=True)
+  net.from_nx(g)
+  net.show("real_world_community_model.html")
---
+++ streamlit.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import random
+import time
+from create_knowledge_graph import generate_response_based_on_question
+
+st.title("Explore Aurvana")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    answer = generate_response_based_on_question(prompt)
+    st.markdown(answer)
