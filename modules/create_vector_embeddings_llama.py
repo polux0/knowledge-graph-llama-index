1b8cb4270b35083980af6f0682372ae48446c54c
polux0 <afidelo@protonmail.com>
2024-03-14 15:19:47 +0100

    feat: single `docker-compose.yaml` as we will migrate to kubernetes for next release;

--- modules/create_vector_embeddings_llama.py
+++ modules/create_vector_embeddings_llama.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from anyio import sleep
 from llama_index.readers.file import PDFReader
 from llama_index.core.response.notebook_utils import display_source_node
 from llama_index.core.retrievers import RecursiveRetriever
@@ -50,12 +51,15 @@ embed_model = initialize_embedding_model(env_vars['HF_TOKEN'], embedding_model_i
 llm = initialize_llm(env_vars['HF_TOKEN'], model_name_id="default")
 
 # initialize ChromaDB
-remote_db = chromadb.HttpClient()
+remote_db = chromadb.HttpClient(host='chromadb')
 
 print("All collections in Chroma: ", remote_db.list_collections())
 
 chroma_collection = remote_db.get_or_create_collection('real_world_community_model')
 chroma_collection_parent = remote_db.get_or_create_collection("real_world_community_model_parent")
+
+print("Are there embeddings inside those collections? real_world_community_model, count: ", chroma_collection.count())
+print("Are there embeddings inside those collections? real_world_community_model_parent, count: ", chroma_collection_parent.count())
 
 vector_store = ChromaVectorStore(chroma_collection=chroma_collection, ssl=False)
 
@@ -66,11 +70,15 @@ storage_context = StorageContext.from_defaults(vector_store=vector_store)
 storage_context_parent = StorageContext.from_defaults(vector_store=vector_store_parent)
 
 # necessary to create a collection for the first time
-# base_index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model)
 
+if chroma_collection.count() == 0:
+
+    base_index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model)
+
+else:
 # after collection was sucessfully created
 
-base_index = VectorStoreIndex.from_vector_store(vector_store, storage_context=storage_context, embed_model=embed_model)
+    base_index = VectorStoreIndex.from_vector_store(vector_store, storage_context=storage_context, embed_model=embed_model)
 
 base_retriever = base_index.as_retriever(similarity_top_k=2)
 
@@ -126,10 +134,15 @@ all_nodes_dict = {n.node_id: n for n in all_nodes}
 print("Finished with making dictionaries")
 
 # necessary to create a collection for the first time
-# vector_index_chunk = VectorStoreIndex(all_nodes, storage_context=storage_context_parent,embed_model=embed_model)
+
+if chroma_collection_parent.count() == 0:
+
+    vector_index_chunk = VectorStoreIndex(all_nodes, storage_context=storage_context_parent,embed_model=embed_model)
+
+else: 
 
 # after collection was sucessfully created
-vector_index_chunk = VectorStoreIndex.from_vector_store(vector_store_parent, storage_context=storage_context_parent, embed_model=embed_model)
+    vector_index_chunk = VectorStoreIndex.from_vector_store(vector_store_parent, storage_context=storage_context_parent, embed_model=embed_model)
 
 vector_retriever_chunk = vector_index_chunk.as_retriever(similarity_top_k = 3)
 
@@ -156,3 +169,8 @@ response = query_engine_chunk.query(
 )
 print("With parent-child retriever enabled****************************************************************")
 print(str(response))
+
+# import time
+
+# while True:
+#     time.sleep(1000) 
