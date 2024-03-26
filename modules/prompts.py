3cc3e25b16b54b27c8e7610cc78717d7fa3651de
polux0 <afidelo@protonmail.com>
2024-03-26 22:46:26 +0100

    feat: prompt change;

--- modules/prompts.py
+++ modules/prompts.py
@@ -9,15 +9,15 @@ TEMPLATES = {
                                 Question: {query}
                                 Helpful Answer:
                                 </s>""",
-    "context": """<|system|>Please check if the following pieces of context has any mention of the keywords provided in the Question.
-                                If yes, please provide the context.
-                                If not then don't know the answer, just say that you don't know.
-                                Stop there.
-                                Please do not try to make up an answer.</s>
+    "simon": """<|system|>Your role is to assist users by navigating through your knowledge bank to find relevant information. First, identify key words in the user's query. Then, search these keywords in your knowledge bank to gather relevant information. Use all gathered information to craft a response that is accurate, informative, engaging, and user-friendly. If the information is not available, clearly state, 'I cannot provide a confident response based on the information I have. Do you have another question?' Invite a relevant response from the user after presenting your answer, to keep the conversation flowing.</s>
                       Question: {query}
                       Answer: </s>""",
     
 }
+
+
+
+# Your role is to assist users by navigating through our documentation to find relevant information. Start with the user's query to identify key words. First, search these keywords in knowledge bank A to gather necessary details. Enhance the query with this information, then proceed to search in knowledge bank B for additional context. Use all gathered information to craft a response that is accurate, informative, engaging, and user-friendly. Invite a relevant response from the user after presenting your answer, to keep the conversation flowing. If the information is not available, clearly state, 'I cannot provide a confident response based on the information I have. Do you have another question?
 def get_template_based_on_template_id(template_id: str):
     """
     Retrieve the template name corresponding to the given template ID.
