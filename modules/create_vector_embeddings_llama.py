59df16803fdf192d4e24c3e7e9a20cfc890422d3
polux0 <afidelo@protonmail.com>
2024-04-24 12:48:34 +0200

    fix: deleting unnecessary comments;

--- modules/create_vector_embeddings_llama.py
+++ modules/create_vector_embeddings_llama.py
@@ -304,14 +304,6 @@ def generate_response_based_on_vector_embeddings_with_debt(question: str):
     print("Template received, vector embeddings:", prompt_template)
     message_template = format_message(question, prompt_template)
     print("!!!!!!!!!!!!!!!!Final question, vetor embeddings:", message_template)
-    # to delete
-    # embedding_model_id = "default"
-    # embed_model = initialize_embedding_model(
-    #     env_vars["HF_TOKEN"], embedding_model_id=embedding_model_id
-    # )
-    # experiment.embeddings_model = get_embedding_model_based_on_model_name_id(
-    #     embedding_model_id
-    # )
     # Settings.embed_model = embed_model
     response = query_engine_chunk.query(message_template)
     # logging.info(f"Logging the response nodes from a vector database: {response.source_nodes}")
