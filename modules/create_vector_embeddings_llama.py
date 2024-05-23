11a1e0215cd8da266fb8d6dc739365c9b4bf13c2
polux0 <afidelo@protonmail.com>
2024-05-23 10:34:56 +0200

    fix: changing name of the collection so we can test redis persistence;

--- modules/create_vector_embeddings_llama.py
+++ modules/create_vector_embeddings_llama.py
@@ -110,7 +110,7 @@ print("All collections in Chroma: ", remote_db.list_collections())
 #     documents_directory
 # )
 
-child_chroma_collection_name = "rwcm1sthalflocaltesting"
+child_chroma_collection_name = "rwcm1sthalflocaltesting1"
 
 print("Child chroma collection name: ", child_chroma_collection_name)
 
