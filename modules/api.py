3142c7d9e4729e94c9084d90c6f4d47d6fe7a53b
polux0 <afidelo@protonmail.com>
2024-06-26 17:25:06 +0200

    feat: elasticsearch logging;

--- modules/api.py
+++ modules/api.py
@@ -1,15 +1,31 @@
-import datetime
+from datetime import datetime, timezone
 from create_raptor_indexing_langchain import generate_response_based_on_raptor_indexing_with_debt
 from flask import Flask, request, jsonify
 from multi_representation_indexing import generate_response_based_on_multirepresentation_indexing_with_debt
+# Elasticsearch
+from elasticsearch_service import ElasticsearchClient, ExperimentDocument
+import logging
+
 
 app = Flask(__name__)
+
+elasticsearch_client = ElasticsearchClient()
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
     data = request.get_json()
+    print(f"Received data in /ask route: {data}")
     question = data.get('question')
+    # Telegram related data
+    telegram_chat_id = data.get('telegram_chat_id')
+    telegram_message_id = data.get('telegram_message_id')
+    telegram_user_id = data.get('telegram_user_id')
+    telegram_user_name = data.get('telegram_user_name')
+    # created_at = data.get('created_at')
+    # Telegram related data
+
     if not question:
         return jsonify({'error': 'No question provided'}), 400
 
@@ -20,70 +36,134 @@ def ask_question():
         f"Raptor agent answer:\n{answer_raptor}\n\n"
         f"MRI agent answer:\n{answer_mri}"
     )
+    # TODO: Modularize
+    additional_fields = {
+        'telegram_chat_id': telegram_chat_id,
+        'telegram_message_id': telegram_message_id,
+        'telegram_user_id': telegram_user_id,
+        'telegram_user_name': telegram_user_name,
+        'document_type': 'message'
+    }
+    # Log interactions
+    elasticsearch_client.save_interaction(experiment_raptor, additional_fields)
+    elasticsearch_client.save_interaction(experiment_mri, additional_fields)
 
-    return jsonify({'answer': formatted_response}), 200
+    return jsonify({
+        'answer': formatted_response,
+        # 'interaction_data_raptor': interaction_data_raptor,
+        # 'interaction_data_mri': interaction_data_mri
+    }), 200
 
 
 @app.route('/rate', methods=['POST'])
 def store_feedback():
-    data = request.get_json()
-    chat_id = data.get('chat_id')
-    user_id = data.get('user_id')
-    user_name = data.get('user_name')
-    message_id = data.get('message_id')
-    feedback_rating = data.get('feedback_rating')
-    feedback_text = data.get('feedback_text')
+    try:
+        data = request.get_json()
+        chat_id = data.get('telegram_chat_id')
+        user_id = data.get('telegram_user_id')
+        user_name = data.get('telegram_user_name')
+        message_id = data.get('telegram_message_id')
+        feedback_rating = data.get('telegram_feedback_rating')
+        feedback_text = data.get('telegram_feedback_text')
 
-    if not all([chat_id, user_id, user_name, message_id]) or (not feedback_rating and not feedback_text):
-        return jsonify({'error': 'Missing required fields'}), 400
+        # Log received data for debugging
+        logging.debug(f"Received data in /rate route: {data}")
+        print(f"Received data in /rate route: {data}")
 
-    # Search for existing feedback document
-    query = {
-        "query": {
-            "bool": {
-                "must": [
-                    {"term": {"telegram_chat_id": chat_id}},
-                    {"term": {"telegram_message_id": message_id}},
-                    {"term": {"telegram_user_id": user_id}},
-                    {"term": {"document_type": "feedback"}}
-                ]
+        if not chat_id:
+            logging.error("Missing telegram_chat_id")
+            print("Missing telegram_chat_id")
+            return jsonify({'error': 'Missing telegram_chat_id'}), 400
+        if not user_id:
+            logging.error("Missing telegram_user_id")
+            print("Missing telegram_user_id")
+            return jsonify({'error': 'Missing telegram_user_id'}), 400
+        if not user_name:
+            logging.error("Missing telegram_user_name")
+            print("Missing telegram_user_name")
+            return jsonify({'error': 'Missing telegram_user_name'}), 400
+        if not message_id:
+            logging.error("Missing telegram_message_id")
+            print("Missing telegram_message_id")
+            return jsonify({'error': 'Missing telegram_message_id'}), 400
+
+        # Check if at least one feedback field is provided
+        if feedback_rating is None and feedback_text is None:
+            logging.error("No feedback provided")
+            print("No feedback provided")
+            return jsonify({'error': 'No feedback provided'}), 400
+
+        # Search for existing feedback document ( for specific user )
+        logging.debug("Searching for the existing feedback")
+        print("Searching for the existing feedback")
+        res = elasticsearch_client.search_feedback(
+            chat_id, message_id, user_id
+        )
+
+        if res['hits']['total']['value'] > 0:
+            # Was used previously, we don't want things to be overrided, so we'll try another approach
+            # Document exists, update it
+            # doc_id = res['hits']['hits'][0]['_id']
+            # existing_feedback = res['hits']['hits'][0]['_source']
+
+            # # Update the feedback fields
+            # if feedback_rating:
+            #     existing_feedback['telegram_feedback_rating'] = feedback_rating
+            # if feedback_text:
+            #     existing_feedback['telegram_feedback_text'] = feedback_text
+
+            # existing_feedback['updated_at'] = datetime.now(timezone.utc)
+            # Testing this approach: 
+            doc_id = res['hits']['hits'][0]['_id']
+            new_feedback_data = {}
+
+            # Update the feedback fields
+            if feedback_rating is not None:
+                new_feedback_data['telegram_feedback_rating'] = feedback_rating
+            if feedback_text is not None:
+                new_feedback_data['telegram_feedback_text'] = feedback_text
+
+            new_feedback_data['updated_at'] = datetime.now(timezone.utc)
+            # Update the document
+            logging.debug("Updating the feedback...")  # Log received data for debugging
+            logging.info("Updating the feedback...")  # Log received data for debugging
+            logging.error("Updating the feedback...")
+            print("Updating the feedback...")  # Log received data for debugging
+            elasticsearch_client.update_feedback(doc_id, new_feedback_data)
+            print(f"Updated feedback from user {user_name} ({user_id}) on message {message_id} in chat {chat_id}: {feedback_rating}, {feedback_text}")
+        else:
+            logging.debug("We need to create new feedback document")
+            print("We need to create new feedback document")
+            # Create a new feedback document
+            feedback_data = {
+                'telegram_chat_id': chat_id,
+                'telegram_message_id': message_id,
+                'telegram_user_id': user_id,
+                'telegram_user_name': user_name,
+                'created_at': datetime.now(timezone.utc),
+                'document_type': 'feedback'
             }
-        }
-    }
-    res = es.search(index='interaction', body=query)
+            if feedback_rating is not None:
+                feedback_data['telegram_feedback_rating'] = feedback_rating
+            if feedback_text is not None:
+                feedback_data['telegram_feedback_text'] = feedback_text
 
-    if res['hits']['total']['value'] > 0:
-        # Document exists, update it
-        doc_id = res['hits']['hits'][0]['_id']
-        existing_feedback = res['hits']['hits'][0]['_source']
+            # Log the feedback data before indexing
+            logging.debug(f"Feedback data to be indexed: {feedback_data}")
+            print(f"Feedback data to be indexed: {feedback_data}")
+            elasticsearch_client.index_document(
+                index='interaction', document=feedback_data
+            )
+            print(f"Created new feedback from user {user_name} ({user_id}) on message {message_id} in chat {chat_id}: {feedback_rating}, {feedback_text}")
 
-        # Update the feedback fields
-        if feedback_rating:
-            existing_feedback['telegram_feedback_rating'] = feedback_rating
-        if feedback_text:
-            existing_feedback['telegram_feedback_text'] = feedback_text
-
-        existing_feedback['updated_at'] = datetime.datetime.utcnow().isoformat()
-
-        # Update the document
-        es.update(index='interaction', id=doc_id, body={"doc": existing_feedback})
-        print(f"Updated feedback from user {user_name} ({user_id}) on message {message_id} in chat {chat_id}: {feedback_rating}, {feedback_text}")
-    else:
-        # Create a new feedback document
-        feedback_data = {
-            'telegram_chat_id': chat_id,
-            'telegram_message_id': message_id,
-            'telegram_user_id': user_id,
-            'telegram_user_name': user_name,
-            'telegram_feedback_rating': feedback_rating,
-            'telegram_feedback_text': feedback_text,
-            'created_at': datetime.datetime.utcnow().isoformat(),
-            'document_type': 'feedback'
-        }
-        es.index(index='interaction', body=feedback_data)
-        print(f"Created new feedback from user {user_name} ({user_id}) on message {message_id} in chat {chat_id}: {feedback_rating}, {feedback_text}")
-
-    return jsonify({'message': 'Feedback processed successfully!'}), 201
+        return jsonify({'message': 'Feedback processed successfully!'}), 201
+    except Exception as e:
+        logging.error(f"Exception occurred: {e}")
+        logging.debug(f"Exception occurred: {e}")
+        logging.info(f"Exception occurred: {e}")  # Log received data for debugging
+        logging.error(f"Exception occurred: {e}")
+        print(f"Exception occurred: {e}")  # Fallback print statement
+        return jsonify({'error': 'Internal Server Error'}), 500
 
 
 app.run(debug=True, port=5000)
--- modules/telegram_bot_first_iteration.py
+++ modules/telegram_bot_first_iteration.py
@@ -36,9 +36,17 @@ async def start(update: Update, context: CallbackContext) -> None:
 async def handle_message(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text
     print(f"Received message: {user_message}")
+    # TODO: Move routes to environment
     ask_response = requests.post(
-        os.getenv("ASK_URL"),
-        json={'question': user_message}
+        "http://127.0.0.1:5000/ask",
+        json={
+            'question': user_message,
+            'telegram_chat_id': update.message.chat.id,
+            'telegram_message_id': update.message.message_id,
+            'telegram_user_id': update.message.from_user.id,
+            'telegram_user_name': update.message.from_user.username,
+            'created_at': update.message.date.isoformat()
+        }
     )
     if ask_response.status_code == 200:
         answer = ask_response.json().get('answer')
@@ -46,53 +54,44 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
     else:
         answer = "Sorry, we could not process your question. Please try again later."
         print(f"Error: {ask_response.text}")
-    print(f"Response: {answer}")
-    await update.message.reply_text(answer, reply_markup=feedback_keyboard())
 
-    interaction_data = {
-        'telegram_chat_id': update.message.chat.id,
-        'telegram_message_id': update.message.message_id,
-        'telegram_user_id': update.message.from_user.id,
-        'telegram_user_name': update.message.from_user.username,
-        'created_at': update.message.date.isoformat(),
-        'document_type': 'message',
-        'question': user_message,
-        'response': answer,
-        'chunk_overlap': None,  # Set these fields according to your actual data
-        'chunk_size': None,
-        'corrected_answer': None,
-        'embeddings_model': None,
-        'experiment_id': None,
-        'LLM_used': None,
-        'max_triplets_per_chunk': None,
-        'prompt_template': None,
-        'retrieval_strategy': None,
-        'retrieved_nodes': None,
-        'satisfaction_with_answer': None,
-        'source_agent': None,
-        'updated_at': None
-    }
-    requests.post("http://127.0.0.1:5000/log_interaction", json=interaction_data)
+    # await update.message.reply_text(answer, reply_markup=feedback_keyboard())
+
+    # context.user_data['original_message_id'] = update.message.message_id
+        # Send the bot's response and store the resulting message object
+    bot_response = await update.message.reply_text(answer, reply_markup=feedback_keyboard())
+
+    # Store the bot's response message ID instead of the user's message ID
+    context.user_data['original_message_id'] = bot_response.message_id
+    context.user_data['original_chat_id'] = update.message.chat.id
 
 
 async def feedback_handler(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     await query.answer()
-    feedback = query.data
-    # Data about the feedback:
+    feedback_data = query.data
+    feedback_rating = int(feedback_data.split('_')[1])
     asked_by_telegram_user_id = query.from_user.id
-    telegram_chat_id = query.message.chat.id
-    telegram_message_id = query.message.message_id
+    # Was previously ( We were having problems with ID discrepancy between /ask and /rate end-points )
+    # telegram_chat_id = query.message.chat.id
+    # telegram_message_id = query.message.message_id
+    # Trying to utilize thread-safe context, so we can resolve previously mentioned discrepancy issues
+    telegram_chat_id = context.user_data.get('original_chat_id')
+    telegram_message_id = context.user_data.get('original_message_id')
+
+    print(f"Feedback in form of rating received for message_id: {telegram_message_id}")
     # Print the required information
     print(f"Asked by User ID: {asked_by_telegram_user_id}, Chat ID: {telegram_chat_id}, Message ID: {telegram_message_id}")
     # TODO: Think if there is a better way to acknowledge the feedback:
     # Pros: It's clean, meaning it doesn't include polute group space with messages
     # Cons: It expires once it's rated, so people cannot change mind later
     # Potentional alternative solution: We could write private message to user
-    await query.edit_message_reply_markup(reply_markup=None)  # Remove the feedback buttons as a signal that we got the feedback.
+
+    # Remove the feedback buttons as a signal that we got the feedback.
+    await query.edit_message_reply_markup(reply_markup=None)
 
     # Store the feedback and message info temporarily
-    context.user_data['feedback'] = feedback
+    context.user_data['feedback_rating'] = feedback_rating
     context.user_data['original_message_id'] = telegram_message_id
     context.user_data['original_chat_id'] = telegram_chat_id
 
@@ -104,25 +103,26 @@ async def feedback_handler(update: Update, context: CallbackContext) -> None:
         'telegram_user_name': query.from_user.username,
         'created_at': query.message.date.isoformat(),
         'document_type': 'feedback',
-        'telegram_feedback': feedback
+        'telegram_feedback_rating': feedback_rating,
     }
-    requests.post("http://127.0.0.1:5000/log_interaction", json=feedback_data)
+    # TODO: Move routes to environment
+    requests.post("http://127.0.0.1:5000/rate", json=feedback_data)
     return ConversationHandler.END  # End the conversation here
 
 
 async def feedback_command(update: Update, context: CallbackContext) -> None:
-    if update.message.reply_to_message and update.message.reply_to_message.message_id == context.user_data.get('original_message_id'):
+    original_message_id = context.user_data.get('original_message_id')
+    print("original_message_id in `feedback_command`: {}".format(original_message_id))
+    original_chat_id = context.user_data.get('original_chat_id')
+    print("update.message.reply_to_message in `feedback_command`: {}".format(update.message.reply_to_message))
+    print("update.message.reply_to_message.message_id in `feedback_command`: {}".format(update.message.reply_to_message.message_id))
+
+    if update.message.reply_to_message and update.message.reply_to_message.message_id == original_message_id:
         additional_feedback = update.message.text[len('/feedback '):]  # Remove the command part
-        user_feedback = context.user_data.get('feedback')
-        original_message_id = context.user_data.get('original_message_id')
-        original_chat_id = context.user_data.get('original_chat_id')
         asked_by_telegram_user_id = update.message.from_user.id
 
-        # Process the collected feedback here
-        print(f"User ID: {asked_by_telegram_user_id}, Chat ID: {original_chat_id}, Original Message ID: {original_message_id}")
-        print(f"Feedback: {user_feedback}, Additional Comments: {additional_feedback}")
+        print(f"Additional feedback from {update.message.from_user.username} for message_id: {original_message_id}")
 
-        # Log the feedback
         feedback_data = {
             'telegram_chat_id': original_chat_id,
             'telegram_message_id': original_message_id,
@@ -130,9 +130,9 @@ async def feedback_command(update: Update, context: CallbackContext) -> None:
             'telegram_user_name': update.message.from_user.username,
             'created_at': update.message.date.isoformat(),
             'document_type': 'feedback',
-            'telegram_feedback': additional_feedback
+            'telegram_feedback_text': additional_feedback
         }
-        response = requests.post("http://127.0.0.1:5000/log_interaction", json=feedback_data)
+        response = requests.post("http://127.0.0.1:5000/rate", json=feedback_data)
 
         if response.status_code == 201:
             await update.message.reply_text("Thank you for your additional feedback!")
