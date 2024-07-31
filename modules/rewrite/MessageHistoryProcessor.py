904f41835ab2b310ec44a17fa85d57fb91ac70e9
polux0 <afidelo@protonmail.com>
2024-07-31 17:32:12 +0200

    fix: changing llm;

--- modules/rewrite/MessageHistoryProcessor.py
+++ modules/rewrite/MessageHistoryProcessor.py
@@ -285,8 +285,8 @@ class MessageHistoryProcessor:
         """
 
         response = client.chat.completions.create(
-            # model="gpt-3.5-turbo",
-            model="gpt-4o",	
+            model="gpt-3.5-turbo",
+            # model="gpt-4o",	
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": prompt},
