f0c44659cb9d98170525f82a6d181ee8757d0eb9
polux0 <afidelo@protonmail.com>
2024-08-08 19:48:19 +0200

    fix: minor!

---
+++ modules/rewrite/retrievers/CustomMultiVectorRetriever.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+from typing import List
+
+from langchain.retrievers import MultiVectorRetriever
+from langchain_core.callbacks import CallbackManagerForRetrieverRun
+from langchain.docstore.document import Document
+
+
+class CustomMultiVectorRetriever(MultiVectorRetriever):
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        """Get documents relevant to a query.
+        Args:
+            query: String to find relevant documents for
+            run_manager: The callbacks handler to use
+        Returns:
+            List of relevant documents
+        """
+        results = self.vectorstore.similarity_search_with_score(
+            query, **self.search_kwargs
+        )
+
+        # Map doc_ids to list of sub-documents, adding scores to metadata
+        id_to_doc = defaultdict(list)
+        for doc, score in results:
+            doc_id = doc.metadata.get("doc_id")
+            if doc_id:
+                doc.metadata["score"] = score
+                id_to_doc[doc_id].append(doc)
+
+        # Fetch documents corresponding to doc_ids, retaining sub_docs in metadata
+        docs = []
+        for _id, sub_docs in id_to_doc.items():
+            docstore_docs = self.docstore.mget([_id])
+            if docstore_docs:
+                if doc := docstore_docs[0]:
+                    doc.metadata["sub_docs"] = sub_docs
+                    docs.append(doc)
+
+        return docs
