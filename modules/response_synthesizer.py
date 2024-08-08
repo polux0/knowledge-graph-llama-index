8845af807552ee91b9551afe8a92797574704230
polux0 <afidelo@protonmail.com>
2024-08-08 20:00:16 +0200

    fix: removing logs!

--- modules/response_synthesizer.py
+++ modules/response_synthesizer.py
@@ -32,7 +32,7 @@ experiment.created_at = current_time.isoformat(timespec='milliseconds')
 def create_nodes_with_score(node_list: List):
     nodes_with_score = []
     for item in node_list:
-        print(f"Score:", )
+        # print(f"Score:", )
         node = TextNode(text=item.page_content)
         node_with_score = NodeWithScore(node=node, score=item.metadata['score'])
         nodes_with_score.append(node_with_score)
